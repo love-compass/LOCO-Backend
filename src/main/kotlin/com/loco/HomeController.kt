11af77805fc6f476aabc365066cc1abbf16c1f24
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-03-25 14:46:15 +0900

    Init: First Commit

---
+++ src/main/kotlin/com/loco/HomeController.kt
@@ -0,0 +1,12 @@
+package com.loco
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+    @GetMapping("/")
+    fun Hello(): String{
+        return "Hello"
+    }
+}
