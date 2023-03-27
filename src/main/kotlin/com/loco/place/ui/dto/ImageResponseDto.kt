c52c292effef5741b18d789372080fa6b7595c42
Chocochip101 <fingercut3822@gmail.com>
2023-03-27 17:51:54 +0900

    Feat: 이미지 검색을 위한 Naver 응답용 Dto

---
+++ src/main/kotlin/com/loco/place/ui/dto/ImageResponseDto.kt
@@ -0,0 +1,10 @@
+package com.loco.place.ui.dto
+
+data class ImageResponseDto(
+    val title: String,
+    val link: String,
+    val thumbnail: String,
+    val sizeheight: String,
+    val sizewidth: String,
+) {
+}
