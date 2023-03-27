2affb47f73d524f91afcb5e5f99fc4fb0771d127
Chocochip101 <fingercut3822@gmail.com>
2023-03-27 15:29:43 +0900

    Feat: Exception 형태 변경

--- src/main/kotlin/com/loco/exception/ExceptionResponse.kt
+++ src/main/kotlin/com/loco/exception/ExceptionResponse.kt
@@ -4,7 +4,5 @@ import lombok.Getter
 
 @Getter
 class ExceptionResponse (var errorCode: String, var message: String){
-    fun of(errorType: ErrorType): ExceptionResponse {
-        return ExceptionResponse(errorType.errorCode, errorType.message)
-    }
+
 }
