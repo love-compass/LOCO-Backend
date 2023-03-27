90203f1fa4bc9fdf81f83fd1371b0f41f0a73fdb
Chocochip101 <fingercut3822@gmail.com>
2023-03-27 15:30:01 +0900

    Feat: LocoException 변경

--- src/main/kotlin/com/loco/exception/LocoException.kt
+++ src/main/kotlin/com/loco/exception/LocoException.kt
@@ -5,8 +5,5 @@ import org.springframework.http.HttpStatus
 
 @Getter
 class LocoException(var httpStatus: HttpStatus, var body: ExceptionResponse): RuntimeException(){
-    fun LocoException(httpStatus: HttpStatus, errorType: ErrorType){
-        this.httpStatus = httpStatus
-        this.body = ExceptionResponse(errorType.errorCode, errorType.message)
-    }
+
 }
