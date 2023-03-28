c0c2c832d16c916926be61c1e95714fca2531caf
Chocochip101 <fingercut3822@gmail.com>
2023-03-28 16:54:34 +0900

    Feat: GptService 추가

---
+++ src/main/kotlin/com/loco/chatgpt/ChatGptService.kt
@@ -0,0 +1,14 @@
+package com.loco.chatgpt
+
+import org.springframework.stereotype.Service
+
+@Service
+class ChatGptService {
+    fun getFullCourse(){
+
+    }
+
+    fun getOneCourse(){
+
+    }
+}
