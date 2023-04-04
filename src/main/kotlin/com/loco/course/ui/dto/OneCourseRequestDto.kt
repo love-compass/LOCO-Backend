e3562119307753616558a780b050f14b6e87e0fa
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-04-04 16:05:49 +0900

    docs: api 명세 설명 추가

--- src/main/kotlin/com/loco/course/ui/dto/OneCourseRequestDto.kt
+++ src/main/kotlin/com/loco/course/ui/dto/OneCourseRequestDto.kt
@@ -1,14 +1,21 @@
 package com.loco.course.ui.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 
 data class OneCourseRequestDto (
     val prior_activity_name: String,
+
+    @ApiModelProperty(example = "강남/역삼/선릉")
     val place:String,
+
+    @ApiModelProperty(example = "2023-01-01T18:00:00")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val start_time: LocalDateTime,
+
+    @ApiModelProperty(example = "2023-01-01T18:00:00")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val end_time: LocalDateTime,
 
