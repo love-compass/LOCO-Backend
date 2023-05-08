1f6fc48c95ae5ae37bf1483f9f66b9873d7d451a
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-05-08 18:28:44 +0900

    Create GooglePlaceCandidates.kt

---
+++ src/main/kotlin/com/loco/place/ui/dto/GooglePlace/GooglePlaceCandidates.kt
@@ -0,0 +1,9 @@
+package com.loco.place.ui.dto.GooglePlace
+
+class GooglePlaceCandidates(
+    val formatted_address: String,
+    val geometry: String,
+    val name: String,
+    val rating: String,
+) {
+}
